3e6cd50045504a3789e113ef6b3381036faafef6
Aymane Akherraz <aymaneakherraz@gmail.com>
2025-04-28 19:51:40 +0100

    Added some checks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aakherra <aakherra@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 20:01:32 by aakherra          #+#    #+#             */
-/*   Updated: 2025/04/28 15:35:09 by aakherra         ###   ########.fr       */
+/*   Updated: 2025/04/28 19:37:59 by aakherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,31 @@ int	is_valid(char *s)
 	return (0);
 }
 
+int	check_qoutes(char *s)
+{
+	int	i;
+	int	single_f;
+	int	double_f;
+
+	single_f = 0;
+	double_f = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] != '\'' && s[i] != '"')
+			i++;
+		if (s[i] == '\'' && !double_f)
+			single_f = !single_f;
+		else if (s[i] == '"' && !single_f)
+			double_f = !double_f;
+		if (s[i])
+			i++;
+	}
+	if (double_f || single_f)
+		return (1);
+	return (0);
+}
+
 int	main(void)
 {
 	//int		i;
@@ -56,7 +81,9 @@ int	main(void)
 		add_history(line);
 		new_line = ft_strtrim(line, " ");
 		if (is_valid(new_line))
-			printf("error\n");
+			printf("error1\n");
+		if (check_qoutes(new_line))
+			printf("error2\n");
 		free(line);
 		free(new_line);
 	}
